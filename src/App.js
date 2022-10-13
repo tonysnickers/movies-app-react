9838b2ac1a04a4c3c9eef14daeadab5df7a0774f
tonysnickers <antoine.mendy@hotmail.fr>
2022-10-14 01:23:00 +0200

    movie genre added

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
       <Navbar/>
       <Routes>
         <Route path='/' element={<Home/>}/>
-        <Route path='movie/:id' element={<MovieDetail/>}/>
+        <Route path='/movie/:id' element={<MovieDetail/>}/>
         <Route path='*' element={"error"}/>
       </Routes>
     </div>
